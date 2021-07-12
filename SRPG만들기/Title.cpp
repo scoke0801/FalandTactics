17637748f35a1ab60a854a7406158dc2856b4f6d
Lim Jong Hyun <scoke0801@gmail.com>
2021-07-12 12:45:38 +0900

    주석 추가, 불필요한 코드 삭제

--- SRPG만들기/Title.cpp
+++ SRPG만들기/Title.cpp
@@ -4,9 +4,6 @@
 CTitleScene::CTitleScene(CFrameWork * framework, HWND hWnd)
 	: CScene(framework, hWnd)
 {
-	//CImage image;
-	//HBITMAP hBmp;
-
 	m_font = CreateFont(30, 10, 0, 0, 0, 0, 0, 0, HANGEUL_CHARSET, 0, 0, 0,
 		VARIABLE_PITCH | FF_ROMAN, _T("굴림"));
 
@@ -15,20 +12,12 @@ CTitleScene::CTitleScene(CFrameWork * framework, HWND hWnd)
 
 	UI_stageSelect = NULL;
 
-	//CreateButton();
-	/*SendMessage(m_buttonSelectStage, WM_SETFONT, (WPARAM)m_font, MAKELPARAM(TRUE, 0));
-	SendMessage(m_buttonLoadStage, WM_SETFONT, (WPARAM)m_font, MAKELPARAM(TRUE, 0));
-	SendMessage(m_buttonEndGame, WM_SETFONT, (WPARAM)m_font, MAKELPARAM(TRUE, 0));*/
-	
 	UI_Login = CUI_Login::GetInstance(m_pFrameWork->GetHWND(),
 		m_pFrameWork->GetHINSTANCE());
 	UI_Login->SetParent(this);
 	UI_Login->SetPos(POINT{ m_rcApp.right / 2 - 150 , m_rcApp.bottom / 2 - 100 });
 	
-	/*if (CProfile::GetInstance() == NULL)
-		UI_Login->MakeUI();
-	else*/
-		CreateButton();
+	CreateButton();
 	CUI_StageSelect::GetInstance(m_pFrameWork->GetHWND(), m_pFrameWork->GetHINSTANCE());
 	CUI_StageSelect::GetInstance()->SetFraemWork(m_pFrameWork);
 	CUI_StageSelect::GetInstance()->SetCurrentStage(CScene::CurrentScene::Title);
@@ -39,35 +28,6 @@ CTitleScene::~CTitleScene()
 
 void CTitleScene::OnUpdate(float timeElapsed)
 {
-	//if (UI_stageSelect != NULL && m_buttonSelectStage != NULL)
-	//{
-	//	if (UI_stageSelect->UpdateUI())
-	//	{
-	//		m_buttonSelectStage = CreateWindow(_T("button"), _T("스테이지 선택"),
-	//			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-	//			m_rcApp.right / 8 * 1, m_rcApp.bottom - 75, m_rcApp.right / 4 * 1, 75,
-	//			m_hWnd, (HMENU)IDC_BUTTON_SelectStage, m_pFrameWork->GetHINSTANCE(), NULL);
-	//
-	//		m_buttonLoadStage = CreateWindow(_T("button"), _T("맵 불러오기"),
-	//			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-	//			m_rcApp.right / 8 * 3, m_rcApp.bottom - 75, m_rcApp.right / 4 * 1, 75,
-	//			m_hWnd, (HMENU)IDC_BUTTON_LoadStage, m_pFrameWork->GetHINSTANCE(), NULL);
-	//
-	//		/*CreateWindow(_T("button"), _T("튜토리얼"),
-	//			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-	//			m_rcApp.left + 600, m_rcApp.bottom - 100, 150, 50,
-	//			hWnd, (HMENU)NULL, m_pFrameWork->GetHINSTANCE(), NULL);*/
-	//
-	//		m_buttonEndGame = CreateWindow(_T("button"), _T("게임 종료"),
-	//			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-	//			m_rcApp.right / 8 * 5, m_rcApp.bottom - 75, m_rcApp.right / 4 * 1, 75,
-	//			m_hWnd, (HMENU)IDC_BUTTON_EndGame, m_pFrameWork->GetHINSTANCE(), NULL);
-	//
-	//		SendMessage(m_buttonSelectStage, WM_SETFONT, (WPARAM)m_font, MAKELPARAM(TRUE, 0));
-	//		SendMessage(m_buttonLoadStage, WM_SETFONT, (WPARAM)m_font, MAKELPARAM(TRUE, 0));
-	//		SendMessage(m_buttonEndGame, WM_SETFONT, (WPARAM)m_font, MAKELPARAM(TRUE, 0));
-	//	}
-	//}
 }
 void CTitleScene::OnDraw(HDC hdc)
 {
@@ -120,6 +80,7 @@ void CTitleScene::CreateButton()
 }
 BOOL CTitleScene::OnProcessingKeyboardMessage(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
+	// 디버깅용 설정!!!!
 	switch (wParam)
 	{
 	case VK_F1:
@@ -197,17 +158,9 @@ BOOL CTitleScene::OnProcessingWindowMessage(HWND hWnd, UINT iMsg, WPARAM wParam,
 			DestroyWindow(m_buttonLoadStage);
 			DestroyWindow(m_buttonEndGame);
 			UI_stageSelect->MakeUI();
-			
-			//UI_stageSelect->DestroyUI();
-			//m_pFrameWork->ChangeScene(CScene::CurrentScene::Stage1);
-			
-		//	UI_stageSelect->SetRect();
 			return TRUE;
-		case IDC_BUTTON_LoadStage:
-			/*DestroyWindow(m_buttonSelectStage);
-			DestroyWindow(m_buttonLoadStage);
-			DestroyWindow(m_buttonEndGame);*/
 
+		case IDC_BUTTON_LoadStage:
 			if (m_pFrameWork->LoadScene())
 			{
 				DestroyWindow(m_buttonSelectStage);
